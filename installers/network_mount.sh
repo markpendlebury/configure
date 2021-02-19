dd6ce1e20ee0a16fd63b423675ded8ec76cf281a
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2021-02-19 09:33:09 +0000

    removed charset

--- installers/network_mount.sh
+++ installers/network_mount.sh
@@ -16,8 +16,8 @@ read -s PASSWORD
 
 printf "username=$USERNAME\npassword=$PASSWORD" > ~/.smbcredentials
 
-sudo printf "//$STORAGEIP/media /media/$USERNAME/media  cifs credentials=/home/$USERNAME/.smbcredentials,iocharset=utf8,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0\n" >> /etc/fstab
-sudo printf "//$STORAGEIP/backup /media/$USERNAME/backup  cifs credentials=/home/$USERNAME/.smbcredentials,iocharset=utf8,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0" >> /etc/fstab
+sudo printf "//$STORAGEIP/media /media/$USERNAME/media  cifs credentials=/home/$USERNAME/.smbcredentials,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0\n" >> /etc/fstab
+sudo printf "//$STORAGEIP/backup /media/$USERNAME/backup  cifs credentials=/home/$USERNAME/.smbcredentials,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0" >> /etc/fstab
 
 sudo mkdir -p /media/$USERNAME/media
 sudo mkdir -p /media/$USERNAME/backup
