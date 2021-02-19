d7183ed16f1e9fc55a7fb3d3a0ba97e78127a4b8
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2021-02-19 09:08:45 +0000

    added -p to mkdir

--- installers/network_mount.sh
+++ installers/network_mount.sh
@@ -19,7 +19,7 @@ printf "username=$USERNAME\npassword=$PASSWORD" > ~/.smbcredentials
 sudo printf "//$STORAGEIP/media /media/$USERNAME/media  cifs credentials=/home/$USERNAME/.smbcredentials,iocharset=utf8,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0\n" >> /etc/fstab
 sudo printf "//$STORAGEIP/backup /media/$USERNAME/backup  cifs credentials=/home/$USERNAME/.smbcredentials,iocharset=utf8,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0" >> /etc/fstab
 
-sudo mkdir /media/$USERNAME/media
-sudo mkdir /media/$USERNAME/backup
+sudo mkdir -p /media/$USERNAME/media
+sudo mkdir -p /media/$USERNAME/backup
 
 sudo mount -a
