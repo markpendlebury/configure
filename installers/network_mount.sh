2b98bc76e053122036768f18e82bb1f400c96588
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2021-02-19 09:38:31 +0000

    added reletive path for credentials file

--- installers/network_mount.sh
+++ installers/network_mount.sh
@@ -14,7 +14,7 @@ echo -n 'Password: '
 read -s PASSWORD
 
 
-printf "username=$USERNAME\npassword=$PASSWORD" > ~/.smbcredentials
+printf "username=$USERNAME\npassword=$PASSWORD" > /home/$USERNAME/.smbcredentials
 
 sudo printf "//$STORAGEIP/media /media/$USERNAME/media  cifs credentials=/home/$USERNAME/.smbcredentials,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0\n" >> /etc/fstab
 sudo printf "//$STORAGEIP/backup /media/$USERNAME/backup  cifs credentials=/home/$USERNAME/.smbcredentials,gid=1000,vers=2.0,uid=1000,file_mode=0777,dir_mode=0777 0 0" >> /etc/fstab
