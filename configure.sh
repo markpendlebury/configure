3544ad4b4bc3b5e3e5822aeb01e956848436dd40
Mark Pendlebury <mark.pendlebury@elesoft.dev>
2021-02-19 10:02:54 +0000

    added checks for each installation step

--- configure.sh
+++ configure.sh
@@ -3,12 +3,40 @@
 export CURRENTSHELL=$(echo $SHELL)
 
 if (CURRENTSHELL != '/usr/bin/zsh'); then
-    echo 'Begin...'
-    sudo ./installers/network_mount.sh
-    sudo ./installers/install_fontpacks.sh
-    sudo ./installers/install_tilix.sh
-    sudo ./installers/install_zsh.sh
+    
+    # Network
+    read -p "Configure Network Shares [y/n]: " -n 1 -r
+    echo   
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo ./installers/network_mount.sh
+    fi
+    
+    # Fonts
+    read -p "Install patched Fonts [y/n]: " -n 1 -r
+    echo    
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo ./installers/install_fontpacks.sh
+    fi
+
+    # Tilix
+    read -p "Install patched Fonts [y/n]: " -n 1 -r
+    echo    
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo ./installers/install_tilix.sh    
+    fi
+    
+    # ZSH
+    read -p "Install patched Fonts [y/n]: " -n 1 -r
+    echo    
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo ./installers/install_zsh.sh
+    fi
+    
 else
-    echo 'Continuing...'
-    sudo ./installers/install_oh_my_zsh.sh
+    # Oh-My-Zsg
+    read -p "Install patched Fonts [y/n]: " -n 1 -r
+    echo    
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo ./installers/install_oh_my_zsh.sh
+    fi
 fi
